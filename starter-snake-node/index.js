21d833283a760c5d3267bccc1d1dbca42172eaa3
Logan Northcote <l.a.northcote@gmail.com>
2019-02-19 23:28:11 -0800

    got snake moving to 0,0. Need to work on switch statements next and determine how to use global vars

--- starter-snake-node/index.js
+++ starter-snake-node/index.js
@@ -21,9 +21,15 @@ app.use(poweredByHandler)
 
 // --- SNAKE LOGIC GOES BELOW THIS LINE ---
 
+
 // Handle POST request to '/start'
 app.post('/start', (request, response) => {
   // NOTE: Do something here to start the game
+console.log(request.body)
+  const height = request.body.board.height
+  console.log(height)
+  const width = request.body.board.width
+  console.log(width)
 
   // Response data
   const data = {
@@ -36,13 +42,39 @@ app.post('/start', (request, response) => {
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
   // NOTE: Do something here to generate your move
+const height = request.body.board.height
+const width = request.body.board.width
+  //Output information
+  //console.log(request.body)
 
+	//GO LEFT
+  //console.log(request.body.you.body[0])
+  if(request.body.you.body[0].x!=0){
   // Response data
-  const data = {
-    move: 'up', // one of: ['up','down','left','right']
+  	const data = {
+		 move: 'left', // one of: ['up','down','left','right']
+  	}
+	console.log('x:')
+	console.log(request.body.you.body[0].x)
+	return response.json(data)
+  }
+  
+
+
+	//GO UP
+  if(request.body.you.body[0].y!=0){
+  // Response data
+  	const data = {
+        	 move: 'up', // one of: ['up','down','left','right']
+  	}
+	console.log('y:')  
+	console.log(request.body.you.body[0].y)
+	return response.json(data)
   }
 
+  const data = {move: 'right',}	
   return response.json(data)
+
 })
 
 app.post('/end', (request, response) => {
