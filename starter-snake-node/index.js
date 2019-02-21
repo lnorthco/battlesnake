51d4d63103f62d4e060b9d36d6fc1aaa40cfbf26
Logan Northcote <l.a.northcote@gmail.com>
2019-02-20 22:12:00 -0800

    got it working! Snakes now detect others, its self and walls. Need to get login now to go for apples

--- starter-snake-node/index.js
+++ starter-snake-node/index.js
@@ -26,10 +26,10 @@ app.use(poweredByHandler)
 app.post('/start', (request, response) => {
   // NOTE: Do something here to start the game
 console.log(request.body)
-  const height = request.body.board.height
-  console.log(height)
-  const width = request.body.board.width
-  console.log(width)
+//  const height = request.body.board.height
+//  console.log(height)
+//  const width = request.body.board.width
+// console.log(width)
 
   // Response data
   const data = {
@@ -42,11 +42,57 @@ console.log(request.body)
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
   // NOTE: Do something here to generate your move
-const height = request.body.board.height
-const width = request.body.board.width
-  //Output information
+  //GET INFORMATION
+var height = request.body.board.height -1
+var width = request.body.board.width -1
+var myBody = request.body.you.body
+//console.log(height)
+//console.log(width)
   //console.log(request.body)
+var head = request.body.you.body[0]
+  //SURVIVAL - Find next viable moves
+var up = true
+var down = true
+var left = true
+var right = true
 
+  //check edges
+if(head.y == '0') up=false
+if(head.y == height) down=false
+if(head.x == '0') left=false
+if(head.y == width) right=false
+
+//Get new possible head locations
+var upNewHead = {x:head.x,y:head.y-1}
+var downNewHead = {x:head.x,y:head.y+1}
+var leftNewHead = {x:head.x-1,y:head.y}
+var rightNewHead = {x:head.x+1,y:head.y}
+
+//Test if new head location will hit any other snakes or its self. Snakes contains your body.	
+snakes = request.body.board.snakes
+//console.log(request.body.board.snakes.length)
+for(j=0;j<snakes.length;j++){
+	snakeBody = snakes[j].body
+	for(i=0;i<snakeBody.length;i++){
+		if(upNewHead.x==snakeBody[i].x && upNewHead.y==snakeBody[i].y){up=false}
+		if(downNewHead.x==snakeBody[i].x && downNewHead.y==snakeBody[i].y){down=false}
+		if(leftNewHead.x==snakeBody[i].x && leftNewHead.y==snakeBody[i].y){left=false}
+		if(rightNewHead.x==snakeBody[i].x && rightNewHead.y==snakeBody[i].y){right=false}
+	}
+}
+
+console.log('down:' + down)
+var data
+if(down){data = {move: 'down',}}
+else if(left){ data = {move: 'left',}}
+else if(up){ data = {move: 'up',}}
+else if(right){ data = {move: 'right',}}
+	
+return response.json(data)
+
+})
+
+/*
 	//GO LEFT
   //console.log(request.body.you.body[0])
   if(request.body.you.body[0].x!=0){
@@ -76,6 +122,8 @@ const width = request.body.board.width
   return response.json(data)
 
 })
+
+*/
 
 app.post('/end', (request, response) => {
   // NOTE: Any cleanup when a game is complete.
